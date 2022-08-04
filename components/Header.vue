c70d64550abc8b63ba6da85851d921f3763c128c
« Matteo <matteo.courquin@gmail.com>
2022-08-04 18:40:10 +0200

    :rocket: init project

---
+++ components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+
+    <header class="flex items-center justify-center">
+      <h1>Template Nuxt</h1>
+    </header>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+header{
+  width: 100%;
+  height: 70px;
+
+  background: $color-black;
+  
+  h1{
+    color: $color-yellow;
+  }
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <Header/>
+</template>
+
+<script>
+export default {
+}
+</script>
